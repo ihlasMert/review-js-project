92df79b5ad53cf793daafed09cfcc17223399908
İHLASMERT <ihlas1mert@gmail.com>
2023-01-05 20:50:53 +0300

    completed project

--- app.js
+++ app.js
@@ -61,8 +61,19 @@ function showPerson(person) {
 
 //show next person
 
-nextBtn.addEventListener("click", function(){
-  currentItem++
-  showPerson(currentItem)
-})
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
 
+//show prev person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1
+  }
+  showPerson(currentItem);
+});
